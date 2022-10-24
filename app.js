fb58b19640309bda9da15d24b64df989251100a8
Cindy Wong <awesome7852@gmail.com>
2022-10-23 22:51:33 -0400

    began quoteSearch function

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log("hi")
+// Constants and Variables
+const baseURL = "https://zenquotes.io/api/"
+
+// api call format: https://zenquotes.io/api/[mode]/[key]?option1=value&option2=value
+
+// function that does quote search based on keyword
+function quoteSearch(keyword){
+    // constructing url for request
+    const url = `${baseURL}quotes/?keyword=${keyword}`
+    console.log(url)
+}
+
+quoteSearch("happiness")
+
+// event listener on form that calls quote search function
