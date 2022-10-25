dd4bb445951c0036bf0bf52e793c1a7bc5031085
Cindy Wong <awesome7852@gmail.com>
2022-10-24 23:56:25 -0400

    tried a different api to construct url

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 // Constants and Variables
-const baseURL = "https://zenquotes.io/api/"
+const apiKey = "d2eb542144mshb55642cc8fd3881p1d80c5jsnc5a89d56f7df"
+const baseURL = "https://healthruwords.p.rapidapi.com/v1/quotes/?"
 
-// api call format: https://zenquotes.io/api/[mode]/[key]?option1=value&option2=value
+// api call format: https://healthruwords.p.rapidapi.com/v1/quotes/?rapidapi-key=d2eb542144mshb55642cc8fd3881p1d80c5jsnc5a89d56f7df&t=Wisdom&maxR=1&size=medium&id=731
 
-// function that does quote search based on keyword
-function quoteSearch(keyword){
+// function that does quote search based on topic
+function quoteSearch(topic){
     // constructing url for request
-    const url = `${baseURL}quotes/?keyword=${keyword}`
+    const url = `${baseURL}rapidapi-key=${apiKey}&t=${topic}&maxR=1&size=medium&id=21`
     console.log(url)
 }
 
-quoteSearch("happiness")
+quoteSearch("Wisdom")
 
 // event listener on form that calls quote search function
