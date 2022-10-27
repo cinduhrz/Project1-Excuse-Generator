260fbeae49e9a5b71b05c2078c650687f8b4a354
Cindy Wong <awesome7852@gmail.com>
2022-10-27 00:23:52 -0400

    added button functionality to render excuse to screen

--- app.js
+++ app.js
@@ -3,16 +3,35 @@ const baseURL = "https://excuser.herokuapp.com/v1"
 
 // api call format: 
 
-// function that gets a random excuse
-function getRandomExcuse(){
+// function that gets a random excuse and renders to screen
+function renderRandomExcuse(){
     // constructing url for request
     const url = `${baseURL}/excuse`
     console.log(url)
+
+    $.ajax(url).then((excuse) => {
+        console.log(excuse)
+
+        // put data on screen
+        const $excuseDisplay = $("div.excuse")
+        $excuseDisplay.html(excuse[0].excuse)
+
+    })
+
+
 }
 
-getRandomExcuse()
+renderRandomExcuse()
 
 // event listener on form that calls quote search function
+$("input[type=submit]").on("click", (e) => {
+    // prevent auto refresh after clicking button
+    e.preventDeafult()
+
+    // update the screen
+    renderRandomExcuse()
+})
+
 
 // https://zenquotes.io/api/quotes/
 
